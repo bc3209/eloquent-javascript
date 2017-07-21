9bfe0063471d71ca68367ad6dfe5330893ca0b7d
Bennett <bchidoro9@gmail.com>
2017-07-20 20:47:28 -0400

    :package: feat Initial commit and add looping-triangle exercise

---
+++ chapter-two-exercises/looping-triangle.js
@@ -0,0 +1,19 @@
+/*
+Write a loop that makes seven calls to console.log to output the following
+triangle:
+__________________________________________________________________________
+#
+##
+###
+####
+#####
+######
+#######
+__________________________________________________________________________
+*/
+var result = "#";
+
+for (var count = 0; count < 7; count++) {
+  console.log(result);
+  result += "#";
+}
