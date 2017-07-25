e9522607c2a5d4dd47e9833e9242e34e6bdee4be
Bennett <bchidoro9@gmail.com>
2017-07-25 19:59:01 -0400

    feat: add minimum.js

---
+++ chapter-three-exercises/minimum.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function min that takes two arguments and returns their minimum
+
+*/
+
+function min (a, b) {
+  if (a < b) {
+    return a
+  } else {
+    return b
+  }
+}
+
+console.log(min(1, 13))
+console.log(min(435, 313))
+console.log(min(934, 943))
